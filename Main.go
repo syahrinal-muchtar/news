542faf6feb0c90502cb9de65563a400d63249470
SM\Syahrinal <syahrinalm21@gmail.com>
2018-12-14 12:59:21 +0700

    cors

--- Main.go
+++ Main.go
@@ -4,7 +4,6 @@ import (
 	"News/Models"
 	"fmt"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
@@ -52,7 +51,7 @@ func main() {
 	// r := Routers.SetupRouter()
 
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(CORSMiddleware())
 
 	// r.Use(cors.New(cors.Config{
 	// 	AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
@@ -82,4 +81,22 @@ func main() {
 
 	// running
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://news148.herokuapp.com")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
